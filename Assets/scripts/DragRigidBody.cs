0462f1036e16223ba0734f8658d1c716b28d4ac4
Abel Saberhagen <abelsaberhagen@gmail.com>
2026-03-29 21:20:03 -0400

    added art

--- Assets/scripts/DragRigidBody.cs
+++ Assets/scripts/DragRigidBody.cs
@@ -1,16 +1,47 @@
-using UnityEngine;
 
-public class DragRigidBody : MonoBehaviour
-{
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragRigidBody : MonoBehaviour {
+    //[SerializedField] private Canvas canvas;
+    private RectTransform rectTransform;
+    private Camera camera;
+    private Rigidbody2D rb;
+    private Vector2 offset;
+    private bool dragging = false;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        camera = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseDown()
     {
-        
+        Debug.Log("mouse down");
+        Vector2 mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+        offset = rb.position - mousePos;
+        dragging = true;
     }
+
+    void OnMouseUp()
+    {
+        Debug.Log("mouse up");
+        dragging = false;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       if (dragging)
+        {
+            Vector2 mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+            rb.MovePosition(mousePos + offset);
+        }
+    }
+
+    
+    
 }
